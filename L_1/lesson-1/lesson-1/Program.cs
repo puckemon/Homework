af33e420a20ddd32ba00ee15c3c5d28c58b1d820
kekw <grishenkofedor@mail.ru>
2021-03-28 22:15:54 +0300

    completed the task of the first lesson

---
+++ L_1/lesson-1/lesson-1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ввведите ваше имя --> ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}, сегодня {DateTime.Now}");
+            Console.ReadKey();
+        }
+    }
+}
